84fec9b06ce849a795f979f2f34c3900e0775bc8
Gaston Bichinque <bichinque@gmail.com>
2022-10-04 17:42:47 -0300

    Agregando codigo py

---
+++ code.py
@@ -0,0 +1,8 @@
+print("hola mundo")
+var1 = int(input("Ingresa un numero: "))
+var2 = int(input("Ingresa otro numero: "))
+print("Ingresaste " + str(var1), str(var2))
+print("La suma es: ", str(var1+var2))
+
+###Precio de Iva a producto valor 100
+
